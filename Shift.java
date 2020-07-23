361ada896249da1c226241920bf80fc498200d79
imagineapril <64987202+imagineapril@users.noreply.github.com>
2020-07-23 20:57:25 +0300

    Add files via upload

---
+++ Shift.java
@@ -0,0 +1,26 @@
+package ru.timokhina.lesson21;
+
+public class Shift {
+    public static void main(String[] args) {
+        int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        print(myArray);
+        shift(myArray);
+        print(myArray);
+
+
+    }
+
+    private static void shift(int[] myArray) {
+        for (int i = 0; i < myArray.length-1; i++) {
+            myArray[i] = myArray[i+1];
+        }
+        myArray[myArray.length-1] = 0;
+    }
+
+    public static void print(int myArray[]) {
+        for (int j = 0; j < myArray.length; j++) {
+            System.out.print(myArray[j]);
+            System.out.print((j < myArray.length - 1) ? " , " : "\n");
+        }
+    }
+}
